b9a018cbd86761c6b64238180cca128f1396ba7a
sopasdigital <rob.hilado@gmail.com>
2018-05-25 19:39:41 +0100

    Added gatsby-plugin-sass to handle Sass processing. Now looks more of a website than some random images placed on a webpage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
         {
             resolve: `gatsby-plugin-typography`,
             options: {
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Navbar from '../components/Navbar';
+import './app.sass'
 
 const TemplateWrapper = ({ children }) => (
     <div>
