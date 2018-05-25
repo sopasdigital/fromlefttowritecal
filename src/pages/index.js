eefd23a529374c2c49e173d6ad267c0616c11cb7
sopasdigital <rob.hilado@gmail.com>
2018-05-25 16:44:12 +0100

    Apply typography-plugin and Typography US Web Design Standards theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        <h1>
+            This is my test content.
+        </h1>
+        <p>
+            More blurbs so that I can see what content is gonna show on my site.
+        </p>
+    </div>
+);
