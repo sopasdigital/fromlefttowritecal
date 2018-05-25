f10afed51c26ef9f4069e7e5332223d9012138da
sopasdigital <rob.hilado@gmail.com>
2018-05-25 18:53:47 +0100

    Created the core pages for the MVP site. These being the Blog page and the Contact page

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default() => (
+    <div>
+        <h1>
+            These are my blog entries.
+        </h1>
+        <p>
+            Eventually these will be a list of blog entries.
+        </p>
+    </div>
+);
---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default () => (
+    <div>
+        <h1>
+            Get in touch with me! Look for me at the following social media platforms:
+        </h1>
+
+        <ul>
+            <li>
+                maybe facebook
+            </li>
+            <li>
+                maybe instagram
+            </li>
+        </ul>
+    </div>
+);
